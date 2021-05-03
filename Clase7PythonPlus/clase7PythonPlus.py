c0db68362f06ededd53d78e62276e831489d174a
TobiMaler21 <62100331+TobiMaler21@users.noreply.github.com>
2021-05-03 18:29:43 -0300

    Update clase7PythonPlus.py

--- Clase7PythonPlus/clase7PythonPlus.py
+++ Clase7PythonPlus/clase7PythonPlus.py
@@ -17,23 +17,22 @@ def save_nombres (data, key):
         with open("file.json", "w") as file:
             json.dump(lista,file, indent=4, ensure_ascii=False)
         
-run():
-        layout = [[sg.Text('Elija que informacion quiere ver')],
-                  [sg.Button(button_text="Top 20 titulos de Netflix con mejor calificacion")],
-                  [sg.Button(button_text="Top 10 Apps de Appstore con mejor calificacion")],
-                  [sg.Cancel()]]
+layout = [[sg.Text('Elija que informacion quiere ver')],
+          [sg.Button(button_text="Top 20 titulos de Netflix con mejor calificacion")],
+          [sg.Button(button_text="Top 10 Apps de Appstore con mejor calificacion")],
+          [sg.Cancel()]]
 
 
-        window = sg.Window('Activivdad por Python +', layout)
+window = sg.Window('Activivdad por Python +', layout)
 
-        while True:
-            event, values = window.read()
-            if event == 'Aplicaciones de Appstore':
+while True:
+        event, values = window.read()
+        if event == 'Aplicaciones de Appstore':
                 data = Goku('./appstore_games.csv','Average User Rating')
                 save_nombres(data,'Name')
 
-            elif event == 'Titulos de Netflix':
+        elif event == 'Titulos de Netflix':
                 data = Goku('./netflix_titles.csv','rating')
                 save_nombres(data,'title')
-            elif event == None or 'Cancel':
+        elif event == None or 'Cancel':
                 break
